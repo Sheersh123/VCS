30a8a31a9e9cacef6497fc2233ab58beba1c5b97
Sheersh <sheershsinha30@gmail.com>
2025-09-26 00:18:44 +0530

    first commit in main branch

--- Sample-Scripts/script1.sh
+++ Sample-Scripts/script1.sh
@@ -3,3 +3,7 @@ echo "=== System Resource Monitor ==="
 echo "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | awk -F'%' '{print $1}')"
 echo "Memory Usage: $(free | grep Mem | awk '{printf("%.2f%%"), $3/$2 * 100.0}')"
 echo "Disk Usage: $(df -h / | awk 'NR==2{printf "%s", $5}')"
+
+
+
+### this is my first commit in main branch
