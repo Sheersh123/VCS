e099a4969daeb1ed50be661891c1f3d667e69834
Sheersh <sheershsinha30@gmail.com>
2025-09-26 00:20:20 +0530

    This is the second commit in dev branch

--- Sample-Scripts/script2.sh
+++ Sample-Scripts/script2.sh
@@ -5,4 +5,8 @@ echo "Generated Password: $password"
 
 
 
+<<<<<<< HEAD
 #this is the third commit in main branch
+=======
+###this is my second commit in dev branch
+>>>>>>> c925589 (This is the second commit in dev branch)
