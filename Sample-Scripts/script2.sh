8e4e379752b6d9571793d7160af3bc29701a7034
Sheersh <sheershsinha30@gmail.com>
2025-09-26 00:33:47 +0530

    Updated-Changes after Conflits

--- Sample-Scripts/script2.sh
+++ Sample-Scripts/script2.sh
@@ -5,8 +5,5 @@ echo "Generated Password: $password"
 
 
 
-<<<<<<< HEAD
 #this is the third commit in main branch
-=======
-###this is my second commit in dev branch
->>>>>>> c925589 (This is the second commit in dev branch)
+###this is my second commit in main branch
